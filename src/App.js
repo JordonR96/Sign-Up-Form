072f871039411de20980449894ed44cb55bbfc2c
JordonR96 <57058115+JordonR96@users.noreply.github.com>
2021-05-24 17:08:09 +0100

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// TODO uncomment import logo from './logo.svg';
 import './App.css';
+import './index.css';  
+
+class SignupForm extends React.Component{
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return(
+
+      <h1>Testing the render</h1>
+
+    );
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        {/*TODO uncomment <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
+      <body>
+        <SignupForm/>
+      </body>
     </div>
   );
 }
